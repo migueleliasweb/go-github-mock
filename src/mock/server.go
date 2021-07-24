2c2782c15182319d01770b79ffaa732f6dd75fea
Miguel Elias dos Santos <migueleliasweb@gmail.com>
2021-07-24 15:32:01 +1000

    Add comments to functions and structs

--- src/mock/server.go
+++ src/mock/server.go
@@ -10,28 +10,21 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// EndpointPattern models the GitHub's API endpoints
 type EndpointPattern = string
 
+// MockBackendOption is used to configure the *mux.router
+// for the mocked backend
 type MockBackendOption func(*mux.Router)
 
-type SequencialReponseHandler struct {
+// FIFOReponseHandler handler implementation that
+// responds to the HTTP requests following a FIFO approach.
+type FIFOReponseHandler struct {
 	Responses [][]byte
 }
 
-type EnforceHostRoundTripper struct {
-	Host                 string
-	UpstreamRoundTripper http.RoundTripper
-}
-
-func (efrt *EnforceHostRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
-	splitHost := strings.Split(efrt.Host, "://")
-	r.URL.Scheme = splitHost[0]
-	r.URL.Host = splitHost[1]
-
-	return efrt.UpstreamRoundTripper.RoundTrip(r)
-}
-
-func (srh *SequencialReponseHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// ServeHTTP implementation of `http.Handler`
+func (srh *FIFOReponseHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if len(srh.Responses) == 0 {
 		http.Error(
 			w,
@@ -52,17 +45,63 @@ func (srh *SequencialReponseHandler) ServeHTTP(w http.ResponseWriter, r *http.Re
 	w.Write(srh.Responses[0])
 }
 
+// EnforceHostRoundTripper rewrites all requests with the given `Host`.
+type EnforceHostRoundTripper struct {
+	Host                 string
+	UpstreamRoundTripper http.RoundTripper
+}
+
+// RoundTrip implementation of `http.RoundTripper`
+func (efrt *EnforceHostRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	splitHost := strings.Split(efrt.Host, "://")
+	r.URL.Scheme = splitHost[0]
+	r.URL.Host = splitHost[1]
+
+	return efrt.UpstreamRoundTripper.RoundTrip(r)
+}
+
+// WithRequestMatch implements a simple FIFO for requests
+// of the given `pattern`.
+//
+// Example:
+//
+// 	WithRequestMatch(
+// 		GetUsersByUsername,
+// 		[][]byte{
+// 			MustMarshal(github.User{
+// 				Name: github.String("foobar"),
+// 			}),
+// 		},
+// 	)
 func WithRequestMatch(
 	pattern EndpointPattern,
-	marshalledResponses [][]byte,
+	responsesFIFO [][]byte,
 ) MockBackendOption {
 	return func(router *mux.Router) {
-		router.Handle(pattern, &SequencialReponseHandler{
-			Responses: marshalledResponses,
+		router.Handle(pattern, &FIFOReponseHandler{
+			Responses: responsesFIFO,
 		})
 	}
 }
 
+// WithRequestMatchHandler implements a request callback
+// for the given `pattern`.
+//
+// Example:
+//
+// 	WithRequestMatchHandler(
+// 		GetOrgsProjectsByOrg,
+// 		func(w http.ResponseWriter, _ *http.Request) {
+// 			w.Write(MustMarshal([]github.Project{
+// 				{
+// 					Name: github.String("mocked-proj-1"),
+// 				},
+// 				{
+// 					Name: github.String("mocked-proj-2"),
+// 				},
+// 			}))
+// 		},
+// 	)
 func WithRequestMatchHandler(
 	pattern EndpointPattern,
 	handler func(w http.ResponseWriter, r *http.Request),
@@ -72,6 +111,7 @@ func WithRequestMatchHandler(
 	}
 }
 
+// MustMarshal helper function
 func MustMarshal(v interface{}) []byte {
 	b, err := json.Marshal(v)
 
@@ -82,6 +122,46 @@ func MustMarshal(v interface{}) []byte {
 	panic(err)
 }
 
+// NewMockedHttpClient creates and configures an http.Client that points to
+// a mocked GitHub's backend API.
+//
+// Example:
+//
+// mockedHttpClient := NewMockedHttpClient(
+// 		WithRequestMatch(
+// 			GetUsersByUsername,
+// 			[][]byte{
+// 				MustMarshal(github.User{
+// 					Name: github.String("foobar"),
+// 				}),
+// 			},
+// 		),
+// 		WithRequestMatch(
+// 			GetUsersOrgsByUsername,
+// 			[][]byte{
+// 				MustMarshal([]github.Organization{
+// 					{
+// 						Name: github.String("foobar123thisorgwasmocked"),
+// 					},
+// 				}),
+// 			},
+// 		),
+// 		WithRequestMatchHandler(
+// 			GetOrgsProjectsByOrg,
+// 			func(w http.ResponseWriter, _ *http.Request) {
+// 				w.Write(MustMarshal([]github.Project{
+// 					{
+// 						Name: github.String("mocked-proj-1"),
+// 					},
+// 					{
+// 						Name: github.String("mocked-proj-2"),
+// 					},
+// 				}))
+// 			},
+// 		),
+// )
+
+// c := github.NewClient(mockedHttpClient)
 func NewMockedHttpClient(options ...MockBackendOption) *http.Client {
 	router := mux.NewRouter()
 
