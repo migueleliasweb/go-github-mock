1bd5bbea632f006315fb484ff8a5f7134d68ac59
Miguel Elias dos Santos <migueleliasweb@gmail.com>
2023-01-27 09:20:42 +1100

    Fix default error formatting for mocked responses

--- src/mock/utils.go
+++ src/mock/utils.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/google/go-github/v41/github"
+	"github.com/google/go-github/v50/github"
 )
 
 // MustMarshal helper function that wraps json.Marshal
