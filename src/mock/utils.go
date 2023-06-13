830a1d0bb1c9811ab20e070be19ecaa533583808
quike <quike.mora@gmail.com>
2023-06-13 15:58:59 -0400

    feat: update go-github to v53

--- src/mock/utils.go
+++ src/mock/utils.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/google/go-github/v52/github"
+	"github.com/google/go-github/v53/github"
 )
 
 // MustMarshal helper function that wraps json.Marshal
