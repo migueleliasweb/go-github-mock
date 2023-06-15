c491f605700b2e2bf8408c5f8a725608e201710c
Miguel Elias dos Santos <migueleliasweb@gmail.com>
2023-06-16 00:15:46 +1000

    Add alpha support for Github enterprise

---
+++ src/mock/server_options_test.go
@@ -0,0 +1,73 @@
+package mock
+
+import (
+	"testing"
+
+	"github.com/google/go-github/v52/github"
+	"github.com/gorilla/mux"
+)
+
+func TestWithRequestMatchEnterprise(t *testing.T) {
+	option := WithRequestMatchEnterprise(
+		GetApp,
+		github.App{
+			Name: func() *string {
+				name := "myapp"
+
+				return &name
+			}(),
+		},
+	)
+
+	router := mux.NewRouter()
+
+	option(router)
+
+	router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+		pathRegexp, err := route.GetPathRegexp()
+
+		if err != nil {
+			t.Fatalf("got error reading path regexp: %s", err)
+		}
+
+		if pathRegexp != "^/api/v3/app$" {
+			t.Errorf("pathRegexp is %s, want ^/api/v3/app$", pathRegexp)
+		}
+
+		return nil
+	})
+}
+
+func TestWithRequestMatchPagesEnterprise(t *testing.T) {
+	option := WithRequestMatchPagesEnterprise(
+		GetOrgsReposByOrg,
+		[]github.Repository{
+			{
+				Name: github.String("repo-A-on-first-page"),
+			},
+			{
+				Name: github.String("repo-B-on-first-page"),
+			},
+		},
+	)
+
+	router := mux.NewRouter()
+
+	option(router)
+
+	router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+		pathRegexp, err := route.GetPathRegexp()
+
+		if err != nil {
+			t.Fatalf("got error reading path regexp: %s", err)
+		}
+
+		expectedRegexp := "^/api/v3/orgs/(?P<v0>[^/]+)/repos$"
+
+		if pathRegexp != expectedRegexp {
+			t.Errorf("pathRegexp is %s, want %s", pathRegexp, expectedRegexp)
+		}
+
+		return nil
+	})
+}
